16fb5540437ce44152e286433d888aa927041683
Michal Nowak <alejajestemmondry@gmail.com.pl>
2015-01-20 20:18:38 +0100

    [gantt] Changed colors, added job labels

--- pyage/jobshop/gantt_generator.py
+++ pyage/jobshop/gantt_generator.py
@@ -5,14 +5,22 @@ import matplotlib.pyplot as plt
 
 class GanttGenerator(object):
     __tasks = {}
-    __colors = "bgrcmyk"
     __N = 50
     __n_machines = 0
+
+    __colors = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
 
     def __init__(self, out_dir='gantt'):
         self.__out_dir = out_dir
         if not path.exists(out_dir):
             makedirs(out_dir)
+        for i in range(len(self.__colors)):
+            r, g, b = self.__colors[i]
+            self.__colors[i] = (r / 255., g / 255., b / 255.)
 
     def add_task(self, machine_nr, job_nr, start_time, duration):
         if machine_nr not in self.__tasks.keys():
@@ -32,6 +40,7 @@ class GanttGenerator(object):
                 color = self.__colors[job_nr % len(self.__colors)]
 
                 plt.hlines(machine_nr, start_time, start_time + duration, colors=color, lw=50)
+                plt.text(start_time + 0.1, machine_nr + 0.1, str(job_nr), bbox=dict(facecolor='white', alpha=1.0)) #fontdict=dict(color='white'))
 
                 if duration + start_time > max_duration:
                     max_duration = duration + start_time
